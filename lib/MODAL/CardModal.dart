40b212dd2ca148092480d878f55b2a8956d1a74c
Akarsh Barar <barar.akarsh8@gmail.com>
2021-07-26 21:48:27 +0530

    Added Model class and Data

---
+++ lib/MODAL/CardModal.dart
@@ -0,0 +1,10 @@
+// You can add Image URLS too
+class CardModal {
+  String heading;
+  String mainHeading;
+  int listenersNumber;
+  int speakersNumber;
+  List<String> speakerNames;
+  CardModal(this.heading, this.mainHeading, this.listenersNumber,
+      this.speakersNumber, this.speakerNames);
+}
---
+++ lib/UTILS/CardData.dart
@@ -0,0 +1,12 @@
+import 'package:clubhoseclone/MODAL/CardModal.dart';
+
+List<CardModal> cardList = [
+  CardModal("Heading 1", "Main Heading 1", 91, 35,
+      ["Speaker 1", "Speaker 2", "Speaker 3", "Speaker 4"]),
+  CardModal("Heading 2", "Main Heading 2", 50, 35,
+      ["Speaker 11", "Speaker 22", "Speaker 33", "Speaker 44"]),
+  CardModal("Heading 2", "Main Heading 2", 50, 35,
+      ["Speaker 11", "Speaker 22", "Speaker 33", "Speaker 44"]),
+  CardModal("Heading 2", "Main Heading 2", 50, 35,
+      ["Speaker 11", "Speaker 22", "Speaker 33", "Speaker 44"])
+];
